f0199c61a2f2b3a12e857705bca9edab003fe405
BillyMaysHere32 <kgetz32@gmail.com>
2023-02-25 15:30:17 -0800

    created route and fetch to connect to backend, getting 500 error

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
-import express from 'express';
+import express, { Router } from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    // client url
+    origin: "http://localhost:5173",
+  })
+);
+
 app.use(express.json({ limit: '50mb' }));
 
+// create api endpoints to connect to frontend
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
